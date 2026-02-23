d99c6f7cafa95217ad9f53fd552c6bee5152ebb1
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-02-23 21:56:28 +0530

    create the AIRequestDto

--- SmartBiz/src/main/java/com/SmartBiz/dto/AIRequestDto.java
+++ SmartBiz/src/main/java/com/SmartBiz/dto/AIRequestDto.java
@@ -4,9 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AIRequestDto {
-    private Long requestId;
+    private Long request_Id;
+    private String prompt;
+    private String response;
+    private Integer tokenUsed;
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
