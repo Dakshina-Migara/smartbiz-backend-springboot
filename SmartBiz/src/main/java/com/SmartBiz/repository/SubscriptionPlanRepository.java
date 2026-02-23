e438476e4d51fd0c368ce8c3c08d94dbeaff7f12
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-02-23 22:01:13 +0530

    create the SubscriptionPlanRepository

---
+++ SmartBiz/src/main/java/com/SmartBiz/repository/SubscriptionPlanRepository.java
@@ -0,0 +1,9 @@
+package com.SmartBiz.repository;
+
+import com.SmartBiz.entity.SubscriptionPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, Long> {
+}
