9aa712d5f2600545b8edcc97e24f576ad31b0ad7
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-02-23 22:00:00 +0530

    create the AdminRepository

---
+++ SmartBiz/src/main/java/com/SmartBiz/repository/AdminRepository.java
@@ -0,0 +1,21 @@
+package com.SmartBiz.repository;
+
+import com.SmartBiz.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Optional<Admin> findByEmail(String email);
+
+    @Query("SELECT a FROM Admin a WHERE a.business.business_id = :businessId")
+    Optional<Admin> findByBusinessId(@Param("businessId") Long businessId);
+
+    Boolean existsByEmail(String email);
+}
