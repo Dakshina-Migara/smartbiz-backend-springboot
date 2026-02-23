b1343e7f53deebfba9c3ca25cb354e6a93ae1367
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-02-23 22:01:31 +0530

    update the config file

--- SmartBiz/src/main/java/com/SmartBiz/security/SecurityConfig.java
+++ SmartBiz/src/main/java/com/SmartBiz/security/SecurityConfig.java
@@ -1,8 +1,9 @@
-package com.SmartBiz.config;
+package com.SmartBiz.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +13,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
