a9a5f2416311d0b060423925169ce6d2cbed1974
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-02-23 23:05:39 +0530

    create the Inventory

---
+++ SmartBiz/src/main/java/com/SmartBiz/entity/Inventory.java
@@ -0,0 +1,30 @@
+package com.SmartBiz.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private Integer stockLevel;
+    private Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "business_id")
+    private Businesses business;
+
+    public Inventory(String productName, Integer stockLevel, Double price) {
+        this.productName = productName;
+        this.stockLevel = stockLevel;
+        this.price = price;
+    }
+}
